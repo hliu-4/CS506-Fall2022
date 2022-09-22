9a5c30831c94977e38d09e68ab6bf7ec00a0462c
Henry Liu <hliu4@bu.edu>
2022-09-22 17:48:15 -0400

    read.py and sim.py works with the tests

--- labs/02-lab/cs506/read.py
+++ labs/02-lab/cs506/read.py
@@ -1,6 +1,22 @@
+import csv
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    list = []
+    row = -1
+    
+    with open(csv_file_path, 'r') as file:
+        lines = csv.reader(file)
+        for line in lines:
+            list.append([])
+            row += 1
+            for value in line:
+                try:
+                    list[row].append(int(value))
+                except:
+                    list[row].append(value)
+
+    return list
